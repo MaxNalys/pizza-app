d4b3879832fcc94881bd49f03a0d08d3a16d887d
Max Nalysnik <max.nalys@gmail.com>
2026-05-19 15:19:36 +0300

    added icons

---
+++ core/designsystem/src/main/java/com/dev/designsystem/icon/PizzaAppIcons.kt
@@ -0,0 +1,14 @@
+package com.dev.designsystem.icon
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.rounded.ArrowBack
+import androidx.compose.material.icons.rounded.FavoriteBorder
+import androidx.compose.material.icons.rounded.Search
+
+
+object PizzaAppIcons {
+    val ArrowBack = Icons.AutoMirrored.Rounded.ArrowBack
+    val Favorite = Icons.Rounded.FavoriteBorder
+
+    val Search = Icons.Rounded.Search
+}
